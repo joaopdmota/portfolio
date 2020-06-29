3463e9991c4b318f319626171840ffff77d75980
joaopdmota <jp.1998.jpd@icloud.com>
2020-06-29 20:44:56 -0300

    Updates

---
+++ precache-manifest.963fdd14f91a97c20c761b04da7b289c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79ff9cfa1398d42c620d0bd1413247e9",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "e95f36a5e71074c452aa",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/react-gh-pages/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95f36a5e71074c452aa",
+    "url": "/react-gh-pages/static/js/main.e8d9b2b6.chunk.js"
+  },
+  {
+    "revision": "cf4a7d08534fbfc02642",
+    "url": "/react-gh-pages/static/js/runtime-main.f54060b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
